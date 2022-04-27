e3e8dcdaa0146d67e8293bace48b4f04f11a2636
Emeka Ude <emekaude@Emekas-MacBook-Air.local>
2022-04-27 18:40:24 +0100

    not loadings

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -1,43 +1,47 @@
 package ie.tudublin;
 
-import processing.core.PApplet;
-import processing.data.TableRow;
-
-public class Nematode {
-    //name
-    String name;
-    //length
-    int length;
-    //limbs
-    int limbs;
-    //gender
-    String gender;
-    //eyes
-    int eyes;
-
-    PApplet pa;
-
-
-
-
-   
-    public String toString() {
-        return "Nematode [eyes=" + eyes + ", gender=" + gender + ", length=" + length + ", limbs=" + limbs + ", name="
-                + name + "]";
-    }
+public class Nematode 
+{
+    private String name;
+    private boolean eyes;  //boolean for true or false
+     private char gender;
+    private boolean limbs;
+    private int length;
     
-    public Nematode(String name, int length, int limbs, String gender, int eyes) {
-        this.name = name;
-        this.length = length;
-        this.limbs = limbs;
-        this.gender = gender;
-        this.eyes = eyes;
+
+  
+   
+    
+    public String toString() 
+    {
+
+        
+        return "Nematode [eyes=" + eyes + ", gender=" + gender + ", length=" + length + ", limbs=" + limbs + ", name="
+             + name + "]";
     }
+
+
+    
+
+    //tablerow constructor
     public Nematode(TableRow tr)
     {
-        this(tr.getString("name"), tr.getInt("length"), tr.getInt("limbs"), tr.getString("gender"), tr.getInt("eyes"));
-        
+        this(tr.getString("name"), tr.getInt("length"), tr.getInt("limbs") == 1, tr.getChar("gender"), tr.getInt("eyes") == 1);
     }
+  
+    
+    //constructor
+    public Nematode(String name, int length, boolean limbs, char gender, boolean eyes) {
+        this.name = name;
+        this.eyes = eyes;
+        this.gender = gender;
+        this.limbs = limbs;
+        this.length = length;
+        
+       
+       
+    }
+    //accessors
     public String getName() {
         return name;
     }
@@ -47,29 +51,26 @@ public class Nematode {
     public int getLength() {
         return length;
     }
+    public boolean isEyes() {
+        return eyes;
+    }
+    public void setEyes(boolean eyes) {
+        this.eyes = eyes;
+    }
+    public char getGender() {
+        return gender;
+    }
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    public boolean isLimbs() {
+        return limbs;
+    }
+    public void getLimbs(boolean limbs) {
+        this.limbs = limbs;
+    }
     public void setLength(int length) {
         this.length = length;
     }
-    public int getLimbs() {
-        return limbs;
-    }
-    public void setLimbs(int limbs) {
-        this.limbs = limbs;
-    }
-    public String getGender() {
-        return gender;
-    }
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-    public int getEyes() {
-        return eyes;
-    }
-    public void setEyes(int eyes) {
-        this.eyes = eyes;
-    }
-
-
-
-
-} 
+  
+}
--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -34,12 +34,12 @@ public class NematodeVisualiser extends PApplet
 		colorMode(HSB);
 		background(0);
 		smooth();	
-		loadNematodes();
-		loadNematodes();
+		loadNematodes(); // load nematodes in list
+		
 
 	}
 	
-
+	ArrayList<Nematode> nems = new ArrayList<>();
 	public void loadNematodes()
 	{
 		Table table = loadTable("nematodes.csv","header");
@@ -54,10 +54,32 @@ public class NematodeVisualiser extends PApplet
 
 	public void draw()
 	{	
-		//right arrow
+
+		background(255);
+		
+		loadNematodes();
+
+		for (Nematode nematode.getName());
+		{
+			nematode.setName(nematode.getName());
+			nematode.setName(TableRow());
+			fill(0, 400, 600);
+			text(nematode.getName(),width/ 2, height / 4);
+		}
+
+		// arrows right and left
+		float arrowhead = 25;
+		stroke(255);
+		//right
 		line(width - width/4, height/2, width - ( (width/4) + width/6), height/2);
 		line(width - width/4, height/2, width - width/4 - arrowhead, height/2 - arrowhead );
+
+		//left
+		line(width/4, height/2, (width/4) + (width/6), height/2);
+		line(width/4, height/2, width/4 + arrowhead, height/2 - arrowhead);
+		line(width/4, height/2, width/4 + arrowhead, height/2 + arrowhead); 
 		
 		
 	}
+	
 }
